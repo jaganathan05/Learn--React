659924f16e92f9916dfc50e30b33ec6ad373e4a9
jaganathan05 <jaganathanv888@gmail.com>
2024-03-12 15:18:51 +0530

    01 create a component

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,29 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <div>March 18 </div>
+        <div>
+          <h2>Food</h2>
+        </div>
+        <div>10</div>
+      </div>
+      <div>
+        <div>March 18 </div>
+        <div>
+          <h2>petrol</h2>
+        </div>
+        <div>100</div>
+      </div>
+      <div>
+        <div>March 18 </div>
+        <div>
+          <h2>Movie</h2>
+        </div>
+        <div>200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
